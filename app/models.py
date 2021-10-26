0fd79c4bb59e98091c79df5cf4a6ff7994a9ce6b
Axel Conceicao <axel@conceicao.io>
2021-10-26 20:43:15 +0200

    Post handling

--- app/models.py
+++ app/models.py
@@ -11,6 +11,9 @@ DEFAULT_PROFILE_PICTURE = '/profile_picture/default.jpg'
 class User(AbstractUser):
     profile_picture = models.ImageField(
         upload_to='profile_picture', default=DEFAULT_PROFILE_PICTURE)
+    
+    def __str__(self):
+        return self.username
 
 
 @receiver(models.signals.post_delete, sender=User)
@@ -21,3 +24,14 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
     # if instance.file:
     #     if os.path.isfile(instance.file.path):
     #         os.remove(instance.file.path)
+
+class Post(models.Model):
+    caption = models.TextField(max_length=1000, blank=True)
+    image = models.ImageField(upload_to="post/")
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+@receiver(models.signals.post_delete, sender=Post)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework.authtoken import views
-from .views import UserViewset, GenericAPIViewset
+from .views import GenericAPIViewset, UserViewset, PostViewset
 
 router = routers.DefaultRouter()
 router.register(r'api', GenericAPIViewset, basename='Generic Endpoints')
 router.register(r'user', UserViewset, basename='Users')
+router.register(r'post', PostViewset, basename='Posts')
 
 urlpatterns = [
     path('', include(router.urls)),
