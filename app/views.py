e47bebe1fab1e169370c2193cca5de977b0b880e
Axel Conceicao <axel@conceicao.io>
2021-11-13 04:35:03 +0100

    Reverse user posts

--- app/views.py
+++ app/views.py
@@ -25,104 +25,105 @@ class GenericAPIViewset(GenericViewSet):
 
     @action(detail=False, methods=['GET'], serializer_class=UserDetailSerializer)
     def whoami(self, request):
-        serializer = UserDetailSerializer(request.user, context={'request': request})
+        serializer = UserDetailSerializer(
+            request.user, context={'request': request})
         return Response(serializer.data)
 
 
 class UserViewset(ListModelMixin, RetrieveModelMixin, CreateModelMixin, DestroyModelMixin, UpdateModelMixin, GenericViewSet):
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
-    parser_classes=(MultiPartParser, FormParser)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    parser_classes = (MultiPartParser, FormParser)
 
     def get_serializer_class(self):
         if self.action in ['retrieve']:
             return UserDetailSerializer
         return UserSerializer
 
-    def update(self, request, pk, format = None, partial = True):
-        queryset=self.get_queryset().filter(id = pk)
+    def update(self, request, pk, format=None, partial=True):
+        queryset = self.get_queryset().filter(id=pk)
         if not len(queryset):
             return Response(status.HTTP_404_NOT_FOUND)
-        user=queryset[0]
+        user = queryset[0]
         if 'username' in request.data:
-            user.username=request.data['username']
+            user.username = request.data['username']
         if 'email' in request.data:
-            user.email=request.data['email']
+            user.email = request.data['email']
         if 'first_name' in request.data:
-            user.first_name=request.data['first_name']
+            user.first_name = request.data['first_name']
         if 'last_name' in request.data:
-            user.last_name=request.data['last_name']
+            user.last_name = request.data['last_name']
         if 'bio' in request.data:
-            user.bio=request.data['bio']
+            user.bio = request.data['bio']
         if 'password' in request.data:
             user.set_password(request.data['password'])
         if 'profile_picture' in request.data:
-            header, base64_data=request.data['profile_picture'].split(
+            header, base64_data = request.data['profile_picture'].split(
                 ';base64,')
-            decoded_file=base64.b64decode(base64_data)
-            file_name=str(uuid.uuid4())
-            file_extension=get_file_extension(file_name, decoded_file)
+            decoded_file = base64.b64decode(base64_data)
+            file_name = str(uuid.uuid4())
+            file_extension = get_file_extension(file_name, decoded_file)
             if user.profile_picture and user.profile_picture.name != DEFAULT_PROFILE_PICTURE:
                 if os.path.isfile(user.profile_picture.path):
                     os.remove(user.profile_picture.path)
-            user.profile_picture=ContentFile(
-                decoded_file, name = file_name + '.' + file_extension)
+            user.profile_picture = ContentFile(
+                decoded_file, name=file_name + '.' + file_extension)
         user.save()
         return Response(status.HTTP_200_OK)
 
-    @ action(detail = True, methods = ['get'])
-    def followers(self, request, pk = None):
-        users=User.objects.filter(id = pk)
+    @action(detail=True, methods=['get'])
+    def followers(self, request, pk=None):
+        users = User.objects.filter(id=pk)
         if len(users) != 1:
-            return Response('Unable to find user', status = 400)
-        user=users[0]
-        queryset=User.objects.filter(following = user)
+            return Response('Unable to find user', status=400)
+        user = users[0]
+        queryset = User.objects.filter(following=user)
 
-        page=self.paginate_queryset(queryset)
+        page = self.paginate_queryset(queryset)
         if page is not None:
-            serializer=UserSerializer(page, many = True)
+            serializer = UserSerializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
-        serializer=UserSerializer(queryset, many = True)
+        serializer = UserSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    @ action(detail = True, methods = ['get'])
-    def following(self, request, pk = None):
-        users=User.objects.filter(id = pk)
+    @action(detail=True, methods=['get'])
+    def following(self, request, pk=None):
+        users = User.objects.filter(id=pk)
         if len(users) != 1:
-            return Response('Unable to find user', status = 400)
-        user=users[0]
-        queryset=user.following.all()
+            return Response('Unable to find user', status=400)
+        user = users[0]
+        queryset = user.following.all()
 
-        page=self.paginate_queryset(queryset)
+        page = self.paginate_queryset(queryset)
         if page is not None:
-            serializer=UserSerializer(page, many = True)
+            serializer = UserSerializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
-        serializer=UserSerializer(queryset, many = True)
+        serializer = UserSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    @ action(detail = True, methods = ['get'])
-    def posts(self, request, pk = None):
-        users=User.objects.filter(id = pk)
+    @action(detail=True, methods=['get'])
+    def posts(self, request, pk=None):
+        users = User.objects.filter(id=pk)
         if len(users) != 1:
-            return Response('Unable to find user', status = 400)
-        user=users[0]
-        queryset=user.post_set.all()
+            return Response('Unable to find user', status=400)
+        user = users[0]
+        queryset = user.post_set.all()
 
-        page=self.paginate_queryset(queryset)
+        page = self.paginate_queryset(queryset)
         if page is not None:
-            serializer=PostSerializer(page, many = True)
+            serializer = PostSerializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
-        serializer=PostSerializer(queryset, many = True)
-        return Response(serializer.data)
+        serializer = PostSerializer(queryset, many=True)
+        return Response(sorted(serializer.data, key=lambda k: k['id'], reverse=True))
 
 
 class PostViewset(ListModelMixin, RetrieveModelMixin, CreateModelMixin, DestroyModelMixin, GenericViewSet):
-    permission_classes=(IsAuthenticated, )
-    queryset=Post.objects.all()
-    serializer_class=PostSerializer
+    permission_classes = (IsAuthenticated, )
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
     def get_serializer_class(self):
         if self.action == 'create':
@@ -130,7 +131,7 @@ class PostViewset(ListModelMixin, RetrieveModelMixin, CreateModelMixin, DestroyM
         return PostSerializer
 
     def get_serializer_context(self):
-        context=super().get_serializer_context()
+        context = super().get_serializer_context()
         if self.action == 'create':
             context.update({"request": self.request})
         return context
