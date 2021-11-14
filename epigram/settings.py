27f1650d3283d15e460a6495afab3dae79644ce5
Axel Conceicao <axel@conceicao.io>
2021-11-14 20:07:51 +0100

    Increase DATA_UPLOAD_MAX_MEMORY_SIZE

--- epigram/settings.py
+++ epigram/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-c%-vyxuv4pwxduftj6@v%sr7^#l#t-bxb_3xz0pt7qdn2fuwnv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 
 ALLOWED_HOSTS = [
     'epigrambe.herokuapp.com',
